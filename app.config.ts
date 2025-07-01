71e44dcfb9d47657c3c74c97203d9a28b6871af3
Iceman8911 <wuchiJSS3@gmail.com>
2025-07-01 22:50:53 +0100

    Enable SSR false and update esbuild target to es2024

--- app.config.ts
+++ app.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
+		build: { target: "es2024" },
+		optimizeDeps: { esbuildOptions: { target: "es2024" } },
+		esbuild: { target: "es2024" },
 	},
-	// server: { esbuild: { options: { target: "esnext" } } },
+	ssr: false,
+	server: { esbuild: { options: { target: "es2024" } } },
 });
