1662c12e31271320fa73c08c50932a98c67fbae1
Iceman8911 <wuchiJSS3@gmail.com>
2025-07-02 10:44:00 +0100

    Refactor navigation with route-based active links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,23 @@
-import { useLocation } from "@solidjs/router";
+import { A } from "@solidjs/router";
+import type { LucideProps } from "lucide-solid";
+import FilesIcon from "lucide-solid/icons/file-stack";
+import HouseIcon from "lucide-solid/icons/house";
 import MenuIcon from "lucide-solid/icons/menu";
+import FavouritesIcon from "lucide-solid/icons/star";
+import { For, type JSX } from "solid-js";
+import { Dynamic } from "solid-js/web";
 
 export default function Nav() {
 	const DRAWER_WIDTH = "w-[60vw] sm:w-45 md:w-60";
-
-	const location = useLocation();
-	const active = (path: string) =>
-		path == location.pathname
-			? "border-sky-600"
-			: "border-transparent hover:border-sky-600";
+	const ROUTE_LINKS = [
+		{ icon: HouseIcon, name: "Home", route: "/" },
+		{ icon: FilesIcon, name: "Files", route: "/files" },
+		{ icon: FavouritesIcon, name: "Favourites", route: "/about" },
+	] as const satisfies ReadonlyArray<{
+		route: string;
+		name: string;
+		icon: (props: LucideProps) => JSX.Element;
+	}>;
 
 	return (
 		<nav class={`drawer sm:drawer-open absolute sm:relative ${DRAWER_WIDTH}`}>
@@ -19,7 +28,7 @@ export default function Nav() {
 					for="my-drawer-2"
 					class="btn btn-primary drawer-button rounded-l-none p-2 sm:hidden"
 				>
-					<MenuIcon aria-description="Show Drawer Icon" />
+					<MenuIcon aria-label="drawer icon" />
 				</label>
 			</div>
 			<div class="drawer-side">
@@ -31,12 +40,16 @@ export default function Nav() {
 				<ul
 					class={`menu bg-base-200 text-base-content min-h-full ${DRAWER_WIDTH} p-4 text-xl font-bold`}
 				>
-					<li>
-						<a href="/">Files</a>
-					</li>
-					<li>
-						<a href="/about">Favourites</a>
-					</li>
+					<For each={ROUTE_LINKS}>
+						{({ icon, name, route }) => (
+							<li class="my-1">
+								<A href={route} activeClass="bg-neutral" end>
+									<Dynamic component={icon} />
+									{name}
+								</A>
+							</li>
+						)}
+					</For>
 				</ul>
 			</div>
 		</nav>
---
+++ src/routes/files.tsx
@@ -0,0 +1,3 @@
+export default function Files() {
+	return <main class="text-center mx-auto text-gray-700 p-4">Files</main>;
+}
