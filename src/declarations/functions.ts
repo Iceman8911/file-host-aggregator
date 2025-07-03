9a19b01b66ddccc75d5d6b0f4fd1c036e1f4d006
Iceman8911 <wuchiJSS3@gmail.com>
2025-07-03 20:10:04 +0100

    feat(functions): add a way to detect an internet connection

--- src/declarations/functions.ts
+++ src/declarations/functions.ts
@@ -1,3 +1,22 @@
 export const generateUUID = <
 	TReturnType = ReturnType<typeof crypto.randomUUID>,
 >() => crypto.randomUUID() as TReturnType;
+
+export async function gIsUserConnectedToInternet(): Promise<boolean> {
+	try {
+		const response = await fetch("https://www.gstatic.com/generate_204", {
+			method: "POST",
+			mode: "no-cors",
+		});
+
+		if (response) return true;
+		else return false;
+	} catch (_) {
+		return false;
+	}
+}
+
+export async function gThrowIfNoInternet(): Promise<void> {
+	if (!(await gIsUserConnectedToInternet()))
+		throw Error("No connection detected.");
+}
