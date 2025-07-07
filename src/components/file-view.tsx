9391b2bf30ae3897884a5baa152af68e941213b6
Iceman8911 <wuchiJSS3@gmail.com>
2025-07-07 11:44:23 +0100

    refactor(file view): extracted inline functions and jsx for reusability and template clarity

--- src/components/file-view.tsx
+++ src/components/file-view.tsx
@@ -15,17 +15,8 @@ import HardDriveIcon from "lucide-solid/icons/hard-drive";
 import InfoIcon from "lucide-solid/icons/info";
 import RefreshIcon from "lucide-solid/icons/refresh-ccw";
 import SearchIcon from "lucide-solid/icons/search";
-import {
-	createEffect,
-	createMemo,
-	createSignal,
-	For,
-	Match,
-	Show,
-	Suspense,
-	Switch,
-} from "solid-js";
-import { createStore, produce } from "solid-js/store";
+import { createMemo, For, Match, Show, Suspense, Switch } from "solid-js";
+import { createStore, produce, type SetStoreFunction } from "solid-js/store";
 import { Dynamic } from "solid-js/web";
 import {
 	FileHost,
@@ -68,6 +59,11 @@ type FilesOrFileHosts =
 	| ({ type: "dir"; name: string } | { type: "file"; file: FileHostFile })[]
 	| FileHostImplementations[]
 	| null;
+
+type FileOrFolderOrFileHost =
+	| { type: "dir"; name: string }
+	| { type: "file"; file: FileHostFile }
+	| FileHostImplementations;
 
 export default function FileView() {
 	const defaultFileViewSettings: Readonly<FileViewSettings> = {
@@ -194,303 +190,345 @@ export default function FileView() {
 		_processedFilesOrFileHosts(),
 	);
 
-	const [fileDetailsDialogData, setFileDetailsDialogData] = createStore<{
+	return (
+		<div class="grid grid-cols-[1fr_32.5%] sm:grid-cols-[1fr_20%] grid-rows-[2.5rem_1fr] gap-4 p-4 size-full *:bg-base-200 *:rounded-field *:w-full">
+			{/* Breadcrumbs bar */}
+			<DirectoryPathBar
+				pathData={fileViewSettings.pathData}
+				setter={setFileViewSettings}
+			/>
+
+			{/* Utility icons */}
+			<UtilityIcons
+				settings={fileViewSettings}
+				settingsSetter={setFileViewSettings}
+			/>
+
+			{/* Folder/File view */}
+			<div class="col-[1_/_3] flex flex-wrap place-content-start gap-4 md:gap-8 p-4 select-none overflow-y-auto">
+				<Suspense
+					fallback={
+						<div class="size-full flex items-center justify-center">
+							<span class="loading loading-spinner loading-xl text-primary scale-200"></span>
+						</div>
+					}
+				>
+					<ListOfFilesAndFoldersAndFileHosts
+						list={displayedFilesOrFileHosts()}
+						settingsSetter={setFileViewSettings}
+					/>
+				</Suspense>
+			</div>
+		</div>
+	);
+}
+
+function DirectoryPathBar(prop: {
+	pathData: FilePathTracker;
+	setter: SetStoreFunction<FileViewSettings>;
+}) {
+	const handleNavigateToFileHostsView = () => {
+		prop.setter(
+			produce((state) => {
+				state.pathData.fileHost = null;
+			}),
+		);
+	};
+
+	const handleNavigateToFileHostRoot = () => {
+		prop.setter(
+			produce((state) => {
+				state.pathData.relativePath = ROOT_PATH;
+			}),
+		);
+	};
+
+	/**
+	 *
+	 * @param pathIndex The index of the path to navigate to in the `relativePath` array.
+	 */
+	const handleNavigateToPath = (pathIndex: number) => {
+		prop.setter(
+			produce((state) => {
+				state.pathData.relativePath = state.pathData.relativePath.slice(
+					0,
+					pathIndex + 1,
+				);
+			}),
+		);
+	};
+
+	return (
+		<div class="breadcrumbs text-sm sm:text-[1.025rem] px-4 text-primary overflow-y-clip scrollbar-thin select-none">
+			<ul class="*:last:font-bold">
+				<li>
+					<button type="button" onClick={handleNavigateToFileHostsView}>
+						<CloudIcon />
+						File Hosts
+					</button>
+				</li>
+
+				<Show when={prop.pathData.fileHost}>
+					{(fileHost) => {
+						const relativePath = () => prop.pathData.relativePath;
+
+						return (
+							<>
+								<li>
+									<button type="button" onClick={handleNavigateToFileHostRoot}>
+										<HardDriveIcon />
+										{fileHost().name}
+									</button>
+								</li>
+
+								<For each={relativePath()}>
+									{(pathFragment, index) => (
+										<li>
+											<Show
+												// Don't apply to the last list item
+												when={relativePath().length - 1 !== index()}
+												fallback={
+													<div class="flex gap-2 place-items-center">
+														<ClosedFolderIcon />
+														{pathFragment}
+													</div>
+												}
+											>
+												<button
+													type="button"
+													onClick={() => handleNavigateToPath(index())}
+												>
+													<OpenedFolderIcon />
+													{pathFragment}
+												</button>
+											</Show>
+										</li>
+									)}
+								</For>
+							</>
+						);
+					}}
+				</Show>
+			</ul>
+		</div>
+	);
+}
+
+function UtilityIcons(prop: {
+	settings: FileViewSettings;
+	settingsSetter: SetStoreFunction<FileViewSettings>;
+}) {
+	const settings = () => prop.settings;
+
+	const handleRefreshFiles = async () => {
+		if (settings().isRefreshing === true) return;
+
+		prop.settingsSetter(
+			produce(async (state) => {
+				state.isRefreshing = true;
+
+				// Refetch the data
+				await state.pathData.fileHost?.downloadFiles();
+
+				state.isRefreshing = false;
+			}),
+		);
+	};
+
+	return (
+		<div class="flex gap-2 justify-center items-center *:btn *:btn-primary *:btn-soft *:btn-sm *:p-1 *:rounded-2xl">
+			<button type="button">
+				<SearchIcon />
+			</button>{" "}
+			<button
+				type="button"
+				disabled={!settings().pathData.fileHost}
+				onClick={handleRefreshFiles}
+			>
+				<Show when={settings().isRefreshing} fallback={<RefreshIcon />}>
+					<span class="loading loading-spinner"></span>
+				</Show>
+			</button>
+		</div>
+	);
+}
+
+function ListOfFilesAndFoldersAndFileHosts(prop: {
+	list: FilesOrFileHosts | undefined;
+	settingsSetter: SetStoreFunction<FileViewSettings>;
+}) {
+	type FileDetailsDialogData = {
 		data: FileHostImplementations | FileHostFile | null;
 		modalId: string;
-	}>({ data: null, modalId: generateUUID() });
+	};
+
+	const [fileDetailsDialogData, setFileDetailsDialogData] =
+		createStore<FileDetailsDialogData>({ data: null, modalId: generateUUID() });
+
+	const handleOpenFileOrDirectoryOrFileHost = (val: FileOrFolderOrFileHost) => {
+		prop.settingsSetter(
+			produce((state) => {
+				if (val instanceof FileHost) {
+					state.pathData.fileHost = val;
+					state.pathData.relativePath = ROOT_PATH;
+				} else if (val.type === "dir") {
+					state.pathData.relativePath = [
+						state.pathData.relativePath,
+						val.name,
+					].flat();
+				}
+			}),
+		);
+	};
+
+	const handleShowFileDetails = (file: FileHostFile) => {
+		setFileDetailsDialogData(
+			produce((state) => {
+				state.data = file;
+			}),
+		);
+		(
+			document.getElementById(fileDetailsDialogData.modalId) as
+				| HTMLDialogElement
+				| undefined
+		)?.showModal();
+	};
+
+	function ContextMenu(prop: { data: FileOrFolderOrFileHost }) {
+		return (
+			<>
+				<Switch>
+					<Match
+						when={
+							(prop.data instanceof FileHost || prop.data.type === "dir") &&
+							prop.data
+						}
+					>
+						{(val) => (
+							<li>
+								<button
+									type="button"
+									onClick={() => handleOpenFileOrDirectoryOrFileHost(val())}
+								>
+									<Show
+										when={val() instanceof FileHost}
+										fallback={<OpenedFolderIcon />}
+									>
+										<HardDriveIcon />
+									</Show>
+									Open
+								</button>
+							</li>
+						)}
+					</Match>
+				</Switch>
+				<li>
+					<button type="button">Bob</button>
+				</li>
+				<li>
+					<button type="button">Bob</button>
+				</li>
+				<li>
+					<button type="button">E</button>
+				</li>
+				<li>
+					<button
+						type="button"
+						onClick={() => {
+							if (prop.data.type === "file") {
+								handleShowFileDetails(prop.data.file);
+							}
+						}}
+					>
+						<InfoIcon />
+						Details
+					</button>
+				</li>
+			</>
+		);
+	}
+
+	function Thumbnail(prop: { data: FileOrFolderOrFileHost }) {
+		return (
+			<Switch>
+				<Match when={prop.data instanceof FileHost && prop.data}>
+					{(val) => (
+						<>
+							<HardDriveIcon />
+							<Dynamic
+								component={gFileHostIcons[val().type]}
+								class="absolute right-0 bottom-0 size-6 opacity-75"
+							/>
+						</>
+					)}
+				</Match>
+
+				<Match when={prop.data.type === "dir"}>
+					<DefaultFolderIcon />
+				</Match>
+
+				<Match when={prop.data.type === "file" && prop.data.file}>
+					{(file) => (
+						<Switch fallback={<UnknownFileIcon />}>
+							<Match when={file().type === FileType.ARCHIVE}>
+								<ArchiveFileIcon />
+							</Match>
+
+							<Match when={file().type === FileType.AUDIO}>
+								<AudioFileIcon />
+							</Match>
+
+							<Match when={file().type === FileType.DOCUMENT}>
+								<DocumentFileIcon />
+							</Match>
+
+							<Match when={file().type === FileType.IMAGE}>
+								<ImageFileIcon />
+							</Match>
+
+							<Match when={file().type === FileType.TEXT}>
+								<TextFileIcon />
+							</Match>
+
+							<Match when={file().type === FileType.VIDEO}>
+								<VideoFileIcon />
+							</Match>
+						</Switch>
+					)}
+				</Match>
+			</Switch>
+		);
+	}
 
 	return (
 		<>
-			<div class="grid grid-cols-[1fr_32.5%] sm:grid-cols-[1fr_20%] grid-rows-[2.5rem_1fr] gap-4 p-4 size-full *:bg-base-200 *:rounded-field *:w-full">
-				{/* Breadcrumbs bar */}
-				<div class="breadcrumbs text-sm sm:text-[1.025rem] px-4 text-primary overflow-y-clip scrollbar-thin select-none">
-					<ul class="*:last:font-bold">
-						<li>
+			<For each={prop.list}>
+				{(val) => {
+					const name = val.type === "file" ? val.file.name(true) : val.name;
+
+					return (
+						<CustomContextMenu
+							closeOnClick={true}
+							contextMenu={<ContextMenu data={val} />}
+						>
 							<button
 								type="button"
-								onClick={(_) =>
-									setFileViewSettings(
-										produce((state) => {
-											state.pathData.fileHost = null;
-										}),
-									)
-								}
+								class="relative flex flex-col justify-center items-center w-20 md:w-25 lg:w-30 h-fit aspect-square btn btn-primary btn-soft text-xs sm:text-sm"
+								title={name}
+								onClick={() => handleOpenFileOrDirectoryOrFileHost(val)}
 							>
-								<CloudIcon />
-								File Hosts
+								<div class="relative size-fit *:first:size-16">
+									<Thumbnail data={val} />
+								</div>
+
+								<p class="font-bold overflow-clip text-ellipsis whitespace-nowrap w-full">
+									{name}
+								</p>
 							</button>
-						</li>
-
-						<Show when={fileViewSettings.pathData.fileHost}>
-							{(fileHost) => {
-								const relativePath = () =>
-									fileViewSettings.pathData.relativePath;
-
-								return (
-									<>
-										<li>
-											<button
-												type="button"
-												onClick={(_) => {
-													setFileViewSettings(
-														produce((state) => {
-															state.pathData.relativePath = ROOT_PATH;
-														}),
-													);
-												}}
-											>
-												<HardDriveIcon />
-												{fileHost().name}
-											</button>
-										</li>
-
-										<For each={relativePath()}>
-											{(pathFragment, index) => (
-												<li>
-													<Show
-														// Don't apply to the last list item
-														when={relativePath().length - 1 !== index()}
-														fallback={
-															<div class="flex gap-2 place-items-center">
-																<ClosedFolderIcon />
-																{pathFragment}
-															</div>
-														}
-													>
-														<button
-															type="button"
-															onClick={(_) => {
-																// Move back to the folder the button represents
-																setFileViewSettings(
-																	produce((state) => {
-																		state.pathData.relativePath =
-																			state.pathData.relativePath.slice(
-																				0,
-																				index() + 1,
-																			);
-																	}),
-																);
-															}}
-														>
-															<OpenedFolderIcon />
-															{pathFragment}
-														</button>
-													</Show>
-												</li>
-											)}
-										</For>
-									</>
-								);
-							}}
-						</Show>
-					</ul>
-				</div>
-
-				{/* Utility icons */}
-				<div class="flex gap-2 justify-center items-center *:btn *:btn-primary *:btn-soft *:btn-sm *:p-1 *:rounded-2xl">
-					<button type="button">
-						<SearchIcon />
-					</button>{" "}
-					<button
-						type="button"
-						disabled={!fileViewSettings.pathData.fileHost}
-						onClick={(_) => {
-							if (fileViewSettings.isRefreshing === true) return;
-
-							setFileViewSettings(
-								produce(async (state) => {
-									state.isRefreshing = true;
-
-									// Refetch the data
-									await state.pathData.fileHost?.downloadFiles();
-
-									state.isRefreshing = false;
-								}),
-							);
-						}}
-					>
-						<Show
-							when={fileViewSettings.isRefreshing}
-							fallback={<RefreshIcon />}
-						>
-							<span class="loading loading-spinner"></span>
-						</Show>
-					</button>
-				</div>
-
-				{/* Folder/File view */}
-				<div class="col-[1_/_3] flex flex-wrap place-content-start gap-4 md:gap-8 p-4 select-none overflow-y-auto">
-					<Suspense
-						fallback={
-							<div class="size-full flex items-center justify-center">
-								<span class="loading loading-spinner loading-xl text-primary scale-200"></span>
-							</div>
-						}
-					>
-						<For each={displayedFilesOrFileHosts()}>
-							{(val) => {
-								const name =
-									val.type === "file" ? val.file.name(true) : val.name;
-
-								return (
-									<CustomContextMenu
-										closeOnClick={true}
-										contextMenu={
-											<>
-												<Switch>
-													<Match
-														when={val instanceof FileHost || val.type === "dir"}
-													>
-														{(_) => (
-															<li>
-																<button
-																	type="button"
-																	onClick={(_) =>
-																		setFileViewSettings(
-																			produce((state) => {
-																				if (val instanceof FileHost) {
-																					state.pathData.fileHost = val;
-																					state.pathData.relativePath =
-																						ROOT_PATH;
-																				} else if (val.type === "dir") {
-																					state.pathData.relativePath = [
-																						state.pathData.relativePath,
-																						val.name,
-																					].flat();
-																				}
-																			}),
-																		)
-																	}
-																>
-																	<Show
-																		when={val instanceof FileHost}
-																		fallback={<OpenedFolderIcon />}
-																	>
-																		<HardDriveIcon />
-																	</Show>
-																	Open
-																</button>
-															</li>
-														)}
-													</Match>
-												</Switch>
-												<li>
-													<button type="button">Bob</button>
-												</li>
-												<li>
-													<button type="button">Bob</button>
-												</li>
-												<li>
-													<button type="button">E</button>
-												</li>
-												<li>
-													<button
-														type="button"
-														onClick={(_) => {
-															if (val.type === "file") {
-																// Open the dialog with the data it should show
-																setFileDetailsDialogData(
-																	produce((state) => {
-																		state.data = val.file;
-																	}),
-																);
-																(
-																	document.getElementById(
-																		fileDetailsDialogData.modalId,
-																	) as HTMLDialogElement | undefined
-																)?.showModal();
-															}
-														}}
-													>
-														<InfoIcon />
-														Details
-													</button>
-												</li>
-											</>
-										}
-									>
-										<button
-											type="button"
-											class="relative flex flex-col justify-center items-center w-20 md:w-25 lg:w-30 h-fit aspect-square btn btn-primary btn-soft text-xs sm:text-sm"
-											title={name}
-											onClick={(_) => {
-												setFileViewSettings(
-													produce((state) => {
-														if (val instanceof FileHost) {
-															state.pathData.fileHost = val;
-															state.pathData.relativePath = ROOT_PATH;
-														} else if (val.type === "dir") {
-															state.pathData.relativePath = [
-																state.pathData.relativePath,
-																val.name,
-															].flat();
-														}
-													}),
-												);
-											}}
-										>
-											{/* <div
-										class="tooltip tooltip-bottom absolute size-full"
-										data-tip={name}
-									> */}
-											<div class="relative size-fit *:first:size-16">
-												<Switch>
-													<Match when={val instanceof FileHost}>
-														<HardDriveIcon />
-														<Dynamic
-															component={gFileHostIcons[(val as FileHost).type]}
-															class="absolute right-0 bottom-0 size-6 opacity-75"
-														/>
-													</Match>
-
-													<Match when={val.type === "dir"}>
-														<DefaultFolderIcon />
-													</Match>
-
-													<Match when={val.type === "file" && val.file}>
-														{(file) => (
-															<Switch fallback={<UnknownFileIcon />}>
-																<Match when={file().type === FileType.ARCHIVE}>
-																	<ArchiveFileIcon />
-																</Match>
-
-																<Match when={file().type === FileType.AUDIO}>
-																	<AudioFileIcon />
-																</Match>
-
-																<Match when={file().type === FileType.DOCUMENT}>
-																	<DocumentFileIcon />
-																</Match>
-
-																<Match when={file().type === FileType.IMAGE}>
-																	<ImageFileIcon />
-																</Match>
-
-																<Match when={file().type === FileType.TEXT}>
-																	<TextFileIcon />
-																</Match>
-
-																<Match when={file().type === FileType.VIDEO}>
-																	<VideoFileIcon />
-																</Match>
-															</Switch>
-														)}
-													</Match>
-												</Switch>
-											</div>
-
-											<p class="font-bold overflow-clip text-ellipsis whitespace-nowrap w-full">
-												{name}
-											</p>
-											{/* </div> */}
-										</button>
-									</CustomContextMenu>
-								);
-							}}
-						</For>
-					</Suspense>
-				</div>
-			</div>
+						</CustomContextMenu>
+					);
+				}}
+			</For>
 
 			{/* Dialogs */}
 			<Show when={fileDetailsDialogData.data}>
