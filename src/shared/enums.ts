07c094a6e3aa086690a713b6cf814e5a48abe7f8
Iceman8911 <wuchiJSS3@gmail.com>
2025-07-21 23:08:02 +0100

    refactor(enums): convert typescript enums to readonly objects

--- src/shared/enums.ts
+++ src/shared/enums.ts
@@ -1,26 +1,33 @@
-export enum FILE_HOSTS {
-	MEGA = "Mega Sync",
-}
+export const FILE_HOSTS = {
+	MEGA: "Mega Sync",
+} as const;
+
+export type FILE_HOSTS = (typeof FILE_HOSTS)[keyof typeof FILE_HOSTS];
 
 /** Categories for the type of a file.
  *
  * Like all "png", "jpg", "webp", etc files are `FileType.IMAGE`
  */
-export enum FILE_TYPE {
-	TEXT = "txt",
-	IMAGE = "img",
-	VIDEO = "vid",
-	AUDIO = "aud",
-	ARCHIVE = "zip",
+export const FILE_TYPE = {
+	TEXT: "txt",
+	IMAGE: "img",
+	VIDEO: "vid",
+	AUDIO: "aud",
+	ARCHIVE: "zip",
 
-	PDF = "pdf",
-	EPUB = "epub",
+	PDF: "pdf",
+	EPUB: "epub",
+
 	/** Generic text document stored in a binary format, e.g `.docx`, `.xlsx`,etc */
-	DOCUMENT = "doc",
-	OTHER = "bin",
-}
+	DOCUMENT: "doc",
+	OTHER: "bin",
+} as const;
+
+export type FILE_TYPE = (typeof FILE_TYPE)[keyof typeof FILE_TYPE];
 
 /** For keeping track of queries */
-export enum QUERY_NAME {
-	IS_CONNECTED = "isConnected",
-}
+export const QUERY_NAME = {
+	IS_CONNECTED: "isConnected",
+};
+
+export type QUERY_NAME = (typeof QUERY_NAME)[keyof typeof QUERY_NAME];
